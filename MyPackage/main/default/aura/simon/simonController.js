ca7cd671ada06a1327753892d4128ce25bf733fc
wyang19a <wyang19a@gmail.com>
2021-07-08 21:26:17 -0400

    Scaffolds game start setup
    1. blinks new trace btn on game start, lets user click on btns.

--- MyPackage/main/default/aura/simon/simonController.js
+++ MyPackage/main/default/aura/simon/simonController.js
@@ -1,8 +1,14 @@
 ({
     onGameBtnClick : function(cmp, event, helper) {
         console.log(event.target.getAttribute('data-id'))
+        helper.assignMove(cmp, event)
     },
     onGameStartClick : function(cmp, event, helper) {
-        helper.initializeGame(cmp, event);
+        helper.createNewGame(cmp, event)
+        helper.initializeGame(cmp, event)
+
+        setTimeout(function() {
+            helper.userTurn(cmp.event)
+        }, 3000)
     }
 })
--- MyPackage/main/default/aura/simon/simonHelper.js
+++ MyPackage/main/default/aura/simon/simonHelper.js
@@ -1,5 +1,5 @@
 ({
-    initializeGame : function(cmp, event) {
+    createNewGame : function(cmp, event) {
         const newGame = cmp.get('c.createNewGame')
         newGame.setCallback(this, function(response) {
             const state = response.getState()
@@ -9,5 +9,36 @@
             }
         })
         $A.enqueueAction(newGame)
+    },
+    initializeGame : function(cmp, event) {
+        let traceList = cmp.get('v.trace');
+        const currentTrace = Math.floor(Math.random() * 4) // => random Integer from 0 to 3
+        cmp.set('v.currentTrace', currentTrace)
+        traceList.push(currentTrace)
+
+        const newTraceBtn = document.querySelectorAll(`[data-id='${currentTrace}']`)[0]
+        console.log(newTraceBtn)
+        newTraceBtn.classList.add('blink')
+        setTimeout(function() {
+            newTraceBtn.classList.remove('blink')
+            this.userTurn(cmp, event)
+        }, 2000)
+        // choose random of 4
+        // update `Game__c` trace attribute
+        // let user choose
+        // compare with trace
+        // if correct, repeat
+        // if not, flag.
+    },
+    // assignMove : function(cmp, event) {
+    //     let traceList = cmp.get('v.trace');
+    //     const currentTrace = Math.floor(Math.random() * 4) // => random Integer from 0 to 3
+    //     cmp.set('v.currentTrace', currentTrace)
+    //     traceList.push(currentTrace)
+    // },
+    userTurn : function(cmp, event) {
+        document.querySelectorAll('.game-btn').forEach(btn => {
+            btn.classList.remove('disabled')
+        })
     }
 })
