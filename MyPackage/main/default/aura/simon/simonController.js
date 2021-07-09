deb3e043b54d6fcd7051588090eaff77f26d9b0f
wyang19a <wyang19a@gmail.com>
2021-07-08 22:09:23 -0400

    Clicking button assigns new trace and blinks in order

--- MyPackage/main/default/aura/simon/simonController.js
+++ MyPackage/main/default/aura/simon/simonController.js
@@ -1,14 +1,21 @@
 ({
     onGameBtnClick : function(cmp, event, helper) {
         console.log(event.target.getAttribute('data-id'))
-        helper.assignMove(cmp, event)
+        helper.incrementStep(cmp, event)
+        let trace = cmp.get('v.trace')
+        if (cmp.get('v.userStep') == trace.length) {
+            helper.compareTraces(cmp, event)
+        }
+        // helper.assignMove(cmp, event)
     },
     onGameStartClick : function(cmp, event, helper) {
+        // Hide start button once game begins
+
         helper.createNewGame(cmp, event)
         helper.initializeGame(cmp, event)
-
+        helper.hideStartBtn(cmp, event)
         setTimeout(function() {
-            helper.userTurn(cmp.event)
-        }, 3000)
+            helper.userTurn(cmp, event)
+        }, 1000)
     }
 })
--- MyPackage/main/default/aura/simon/simonHelper.js
+++ MyPackage/main/default/aura/simon/simonHelper.js
@@ -15,14 +15,13 @@
         const currentTrace = Math.floor(Math.random() * 4) // => random Integer from 0 to 3
         cmp.set('v.currentTrace', currentTrace)
         traceList.push(currentTrace)
-
-        const newTraceBtn = document.querySelectorAll(`[data-id='${currentTrace}']`)[0]
-        console.log(newTraceBtn)
-        newTraceBtn.classList.add('blink')
-        setTimeout(function() {
-            newTraceBtn.classList.remove('blink')
-            this.userTurn(cmp, event)
-        }, 2000)
+        this.playTraces(cmp, event)
+        // const newTraceBtn = document.querySelectorAll(`[data-id='${currentTrace}']`)[0]
+        // console.log(newTraceBtn)
+        // newTraceBtn.classList.add('blink')
+        // setTimeout(function() {
+        //     newTraceBtn.classList.remove('blink')
+        // }, 1000)
         // choose random of 4
         // update `Game__c` trace attribute
         // let user choose
@@ -30,15 +29,66 @@
         // if correct, repeat
         // if not, flag.
     },
-    // assignMove : function(cmp, event) {
-    //     let traceList = cmp.get('v.trace');
-    //     const currentTrace = Math.floor(Math.random() * 4) // => random Integer from 0 to 3
-    //     cmp.set('v.currentTrace', currentTrace)
-    //     traceList.push(currentTrace)
-    // },
+    assignMove : function(cmp, event) {
+        let traceList = cmp.get('v.trace');
+        const currentTrace = Math.floor(Math.random() * 4) // => random Integer from 0 to 3
+        cmp.set('v.currentTrace', currentTrace)
+        traceList.push(currentTrace)
+    },
     userTurn : function(cmp, event) {
         document.querySelectorAll('.game-btn').forEach(btn => {
             btn.classList.remove('disabled')
         })
+    },
+    hideStartBtn : function(cmp, event) {
+        document.querySelector('.start-btn').style.display = 'none'
+    },
+    incrementStep : function(cmp, event) {
+        let step = cmp.get('v.userStep')
+        step++
+        cmp.set('v.userStep', step)
+    },
+    compareTraces : function(cmp, event) {
+        let trace = cmp.get('v.trace')
+        if (cmp.get('v.userStep') == trace.length) {
+        console.log('correct, and to next step')
+        this.initializeGame(cmp, event)
+        setTimeout(function() {
+            this.userTurn(cmp, event)
+        }, 1000)
+    }
+        // let trace = cmp.get('v.trace')
+        // if (event.target.getAttribute('data-id') == trace[trace.length - 1]) {
+        //     if (cmp.get('v.userStep') == trace.length) {
+        //         console.log('correct, and to next step')
+        //         this.initializeGame(cmp, event)
+        //         setTimeout(function() {
+        //             this.userTurn(cmp, event)
+        //         }, 1000)
+        //     } else {
+        //         console.log('correct, but not next step yet')
+        //     }
+        //     // correct
+        //     // assign new trace, add to list and let user choose again.
+        // } else {
+        //     console.log('incorrect!')
+        //     // Game over!
+        // }
+    },
+    playTraces : function(cmp, event) {
+        let trace = cmp.get('v.trace')
+        for(let i = 0; i < trace.length; i++) {
+            const newTraceBtn = document.querySelectorAll(`[data-id='${trace[i]}']`)[0]
+            setTimeout(function() {
+                newTraceBtn.classList.add('blink')
+                setTimeout(function() {
+                    newTraceBtn.classList.remove('blink')
+                }, 1000)
+            }, 1000 * i)
+            // newTraceBtn.classList.add('blink')
+            // setTimeout(function(i) {
+            //     newTraceBtn.classList.remove('blink')
+            // }, 1000 * i)
+        }
     }
 })
